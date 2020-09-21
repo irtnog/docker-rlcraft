713584b07c50b3da29347df766bd55b2053bf548
Matthew X. Economou <xenophon@irtnog.org>
2020-09-21 10:05:34 -0400

    style(entrypoint): put JVM flags on lines by themselves
    Improve readability by breaking the long `java` command into multiple
    lines.

--- start.sh
+++ start.sh
@@ -15,4 +15,24 @@ fi
 echo " **********************"
 echo " *  STARTING RLCraft  *"
 echo " **********************"
-java -Xmx${JVM_XMX:-4G} -Xms${JVM_XMS:-4G} -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar forge-server.jar nogui
+java -Xmx${JVM_XMX:-4G} -Xms${JVM_XMS:-4G} \
+     -XX:+ParallelRefProcEnabled \
+     -XX:MaxGCPauseMillis=200 \
+     -XX:+UnlockExperimentalVMOptions \
+     -XX:+DisableExplicitGC \
+     -XX:+AlwaysPreTouch \
+     -XX:G1NewSizePercent=30 \
+     -XX:G1MaxNewSizePercent=40 \
+     -XX:G1HeapRegionSize=8M \
+     -XX:G1ReservePercent=20 \
+     -XX:G1HeapWastePercent=5 \
+     -XX:G1MixedGCCountTarget=4 \
+     -XX:InitiatingHeapOccupancyPercent=15 \
+     -XX:G1MixedGCLiveThresholdPercent=90 \
+     -XX:G1RSetUpdatingPauseTimePercent=5 \
+     -XX:SurvivorRatio=32 \
+     -XX:+PerfDisableSharedMem \
+     -XX:MaxTenuringThreshold=1 \
+     -Dusing.aikars.flags=https://mcflags.emc.gs \
+     -Daikars.new.flags=true \
+     -jar forge-server.jar nogui
